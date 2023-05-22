c7fa2cfcd0e245cdc6af95f7179a511c55d03c5d
dluman <dluman@allegheny>
2023-05-22 13:21:24 -0400

    Don't copy broken setup files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="chompchange",
     version="0.1",
-    packages=['chompchange']
+    packages=['chompchange'],
     include_package_data=True,
     description='Global blockchain supporting term-world.',
     long_description=open('README.md', 'r').read(),
