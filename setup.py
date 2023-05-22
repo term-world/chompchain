7f7926b1755658936145eb7eb4498e3c0528b430
dluman <dluman@allegheny>
2023-05-22 13:19:46 -0400

    Adding module structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="chompchange",
+    version="0.1",
+    packages=['chompchange']
+    include_package_data=True,
+    description='Global blockchain supporting term-world.',
+    long_description=open('README.md', 'r').read(),
+    install_requires=[line.strip() for line in open('requirements.txt', 'r').readlines()]
+ )
