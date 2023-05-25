f5f9c7012e080a18ac4cc46ada3feb004ba0efcf
dluman <dluman@allegheny>
2023-05-25 15:45:17 -0400

    Adding notes to transactions

--- chompchain/transaction.py
+++ chompchain/transaction.py
@@ -5,6 +5,13 @@ class Transaction:
     def __init__(self, **kwargs):
         for arg in kwargs:
             setattr(self, arg, kwargs[arg])
+        # Transactions which are not coin exchange should
+        # "sign themselves" in order to guarantee integrity
+
+        # The plan here would be for each node to have a private
+        # hash or key generated on startup which would then sign
+        # transactions and somehow transmit its public key with 
+        # the block to network for verification
 
     def __str__(self):
         return json.dumps(self.__dict__)
