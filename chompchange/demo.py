a185e84c035294a4bad180077d82e19d4dd47c7b
CalebKendra <kendra01@allegheny.edu>
2023-05-24 20:00:52 -0400

    greated good demo

--- chompchange/demo.py
+++ chompchange/demo.py
@@ -1,42 +1,27 @@
-from pymerkle import MerkleTree, verify_inclusion, verify_consistency
-import json
+import getpass
+from tree import Tree
 
-tree = MerkleTree()
+demo_tree = Tree()
 
-# Populate tree with some entries
-for data in ['foo', 'bar', 'baz', 'qux', 'quux']:
-    tree.append_entry(data)
+while True:
+    tx = input("String to add (hit `enter` to exit): ")
+    tx = {
+        "data": tx,
+        "user": getpass.getuser()
+    }
+    if not tx["data"]: break
+    demo_tree.append_data(tx)
 
-# Prove and verify inclusion of `bar`
-proof = tree.prove_inclusion('bar')
-verify_inclusion('bar', tree.root, proof)
+demo_tree.pickle_data()
 
-#I think we would have it so the program always used `prove_consistancy` instead of `prove_inclusion`
-#This way we would be proving the trees are consistant with one another
-#This line creates a serialized version of the proof and store it in a theoretical json that would be couchdb
-#we would need to create another file or way to store the tree itself
-serialized = proof.serialize()
-with open('demo_data.json', 'w') as f:
-  json.dump(serialized,f,indent = 2)
+demo_tree.unpickle_data()
 
-# Save current state
-sublength = tree.length
-subheight = tree.height
-subsize = tree.size
-subroot = tree.root
-
-# Append further entries
-for data in ['corge', 'grault', 'garlpy']:
-    tree.append_entry(data)
-
-# Prove and verify previous state before these entries were made in order to verify consistancy
-proof = tree.prove_consistency(sublength, subroot)
-verify_consistency(subroot, tree.root, proof)
-
-# Print out the root and all leaves of the tree 
-print("Root of Tree:\n",tree.root, "\nLeaves of the Tree:")
-for x in range(tree.length):
-    print(tree.leaf(x))
-
-deserialized = proof.deserialize(serialized)
-print(deserialized)
+while True:
+    data = input("Data entered: ")
+    if not data: break
+    user = input("User entered: ") or getpass.getuser()
+    value = {
+        "data": data,
+        "user": user
+    }
+    demo_tree.is_included(value)
